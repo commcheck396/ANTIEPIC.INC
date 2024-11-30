773120740a5b965ce5598e79bb29586bca2a7b99
commcheck396 <comm_check@qq.com>
2024-11-30 10:00:22 -0800

    sample

--- src/formatDataset.py
+++ src/formatDataset.py
@@ -6,14 +6,16 @@ import subprocess
 # fetch all the files under the path ../data/
 path = '../data/'
 files = os.listdir(path)
-SAMPLE_SIZE = 1000
+files.sort()
+SAMPLE_SIZE = 2000
+sampledUsers = set()
+sampledGames = set()
 for file in files:
     if not os.path.isdir(file):
         if file == 'steam_games.json':
             with open(path + file, 'r') as f:
                 lines = f.readlines()
-            if len(lines) > SAMPLE_SIZE:
-                lines = lines[:SAMPLE_SIZE]
+            lines.sort()
             data = ['[']
             for line in lines:
                 # replace all the True to true
@@ -24,6 +26,15 @@ for file in files:
                 line = re.sub(r"u'", "'", line)
                 # replace all the u" to "
                 line = re.sub(r'u"', '"', line)
+                # get the game_id
+                # pattern: 'id': '761140'
+                curGame = re.search(r"'id': '(.+?)'", line)
+                if curGame:
+                    curGame = curGame.group(1)
+                    if curGame not in sampledGames:
+                        continue
+                else:
+                    continue
                 data.append(line + ',')
             data[-1] = data[-1][:-1]
             data.append(']')
@@ -32,6 +43,7 @@ for file in files:
         elif file == 'australian_users_items.json':
             with open(path + file, 'r') as f:
                 lines = f.readlines()
+            lines.sort()
             if len(lines) > SAMPLE_SIZE:
                 lines = lines[:SAMPLE_SIZE]
             proUsers = []
@@ -39,7 +51,7 @@ for file in files:
             for line in lines:
                 # if line is longer than 1000, record the user_id and steam_id to file ../data/pro_user.json, then skip this line
                 # Format: 'user_id': 'btuty8trgt', 'items_count': 99, 'steam_id': '76561198054746068', 'user_url': 'http://steamcommunity.com/id/btuty8trgt',
-                if len(line) > 1000:
+                if len(line) > 2500:
                     user_id = re.search(r"'user_id': '(.+?)'", line).group(1)
                     steam_id = re.search(r"'steam_id': '(.+?)'", line).group(1)
                     proUsers.append([user_id, steam_id])
@@ -48,6 +60,14 @@ for file in files:
                 line = re.sub(r'True', 'true', line)
                 # replace all the False to false
                 line = re.sub(r'False', 'false', line)
+                # pattern: 'user_id': '76561198028895010'
+                curUser = re.search(r"'user_id': '(.+?)'", line)
+                if curUser:
+                    curUser = curUser.group(1)
+                    if curUser not in sampledUsers:
+                        continue
+                else:
+                    continue
                 data.append(line + ',')
             data[-1] = data[-1][:-1]
             data.append(']')
@@ -59,10 +79,21 @@ for file in files:
         else:
             with open(path + file, 'r') as f:
                 lines = f.readlines()
+            lines.sort()
             if len(lines) > SAMPLE_SIZE:
                 lines = lines[:SAMPLE_SIZE]
             data = ['[']
             for line in lines:
+                # pattern: 'user_id': '76561197970982479'
+                curUser = re.search(r"'user_id': '(.+?)'", line)
+                if curUser:
+                    curUser = curUser.group(1)
+                    sampledUsers.add(curUser)
+                # pattern: 'item_id': '1250'
+                curGame = re.findall(r"'item_id': '(.+?)'", line)
+                if curGame:
+                    for game in curGame:
+                        sampledGames.add(game)
                 # replace all the True to true
                 line = re.sub(r'True', 'true', line)
                 # replace all the False to false
