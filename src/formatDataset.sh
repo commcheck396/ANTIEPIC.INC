ff99b3b8862d04f6fe4f4840fae19f56292c524f
commcheck396 <comm_check@qq.com>
2024-11-30 16:27:57 -0800

    all dataset formated

--- src/formatDataset.sh
+++ src/formatDataset.sh
@@ -1,3 +1,3 @@
-sed -i -e "s/'/@TEMP@/g; s/\"/'/g; s/@TEMP@/\"/g" ../data/australian_user_reviews.json
-sed -i -e "s/'/@TEMP@/g; s/\"/'/g; s/@TEMP@/\"/g" ../data/australian_users_items.json
-sed -i -e "s/'/@TEMP@/g; s/\"/'/g; s/@TEMP@/\"/g" ../data/steam_games.json
+sed -i "s/'/\"/g" ../data/australian_user_reviews.json
+sed -i "s/'/\"/g" ../data/australian_users_items.json
+sed -i "s/'/\"/g" ../data/steam_games.json
